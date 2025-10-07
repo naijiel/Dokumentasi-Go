7b4376741a2ffdb6df3db21598ee1897dd43f4a3
Naila <zahwanaila27@gmail.com>
2025-10-07 08:20:53 -0700

    Create StudyCase1.go

---
+++ StudyCase1.go
@@ -0,0 +1,12 @@
+// Seorang kasir di toko kecil harus menghitung total belanja pelanggan yang
+// membeli berbagai barang dengan harga berbeda. Kadang pelanggan mendapat potongan
+// harga berdasarkan total belanja atau jenis barang tertentu. Setelah transaksi selesai,
+// kasir perlu menampilkan ringkasan pembelian beserta keterangan
+// apakah pelanggan mendapatkan diskon atau tidak.
+package main
+
+import "fmt"
+
+func main () {
+  
+}
