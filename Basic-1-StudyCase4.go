a8216b2de9fc7a36b08fbbca48104d13975abf24
Naila <zahwanaila27@gmail.com>
2025-10-07 08:30:42 -0700

    Create Basic-1-StudyCase4.go

---
+++ Basic-1-StudyCase4.go
@@ -0,0 +1,13 @@
+// Kamu membuat permainan sederhana di mana komputer menyimpan
+// sebuah angka rahasia, dan pemain harus menebaknya. Pemain
+// diberikan kesempatan terbatas untuk mencoba, dan setiap tebakan
+// akan mendapatkan respon apakah terlalu tinggi, terlalu rendah,
+// atau tepat. Setelah permainan selesai, sistem menampilkan jumlah percobaan yang dilakukan.
+
+package main
+
+import "fmt"
+
+func main () {
+  
+}
